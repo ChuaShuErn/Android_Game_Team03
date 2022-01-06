27fc918b610216ac19ceed2a7e5ff1ccf1ef8dd6
ChuaShuErn <shuern.chua@gmail.com>
2022-01-06 08:35:47 +0800

    First Commit of Android Game

---
+++ app/src/main/java/iss/workshop/android_game_t3/MainActivity.java
@@ -0,0 +1,58 @@
+package iss.workshop.android_game_t3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+            Button fetchBtn = findViewById(R.id.btnFetch);
+            Button playBtn = findViewById(R.id.btnPlay);
+
+            //set onClickListener
+            if (fetchBtn != null) {
+                fetchBtn.setOnClickListener(this);
+            }
+
+            if (playBtn != null) {
+                playBtn.setOnClickListener(this);
+            }
+        }
+
+        @Override
+        public void onClick(View view) {
+            int id = view.getId();
+
+            if (id == R.id.btnFetch) {
+
+
+                //image adapter
+                Intent intent = new Intent(this, FetchActivity.class);
+
+                startActivity(intent);
+
+            }
+
+            if (id == R.id.btnPlay) {
+
+
+                //image adapter
+                Intent intent = new Intent(this, PlayActivity.class);
+
+                startActivity(intent);
+
+            }
+
+        }
+    }
+
+
