6ca29111b9a9fee1de8d6a04543169fe545b1213
manuelseet <manuelseet@gmail.com>
2022-01-07 08:42:58 +0800

    fetch layout & prep fetch onclick

--- app/src/main/java/iss/workshop/android_game_t3/FetchActivity.java
+++ app/src/main/java/iss/workshop/android_game_t3/FetchActivity.java
@@ -3,6 +3,8 @@ package iss.workshop.android_game_t3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FetchActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class FetchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fetch);
+
+        Button fetchBtn = findViewById(R.id.btnFetch);
+        fetchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
+
+
 }
