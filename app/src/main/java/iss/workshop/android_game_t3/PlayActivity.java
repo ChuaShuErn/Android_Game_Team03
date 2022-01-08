c1e1640297020980feddbc97793437ec097640e4
Yu Jing <e0703487@u.nus.edu>
2022-01-08 23:53:25 +0800

    Integrate with PlayActivity

--- app/src/main/java/iss/workshop/android_game_t3/PlayActivity.java
+++ app/src/main/java/iss/workshop/android_game_t3/PlayActivity.java
@@ -8,6 +8,7 @@ import android.app.AlertDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.ColorStateList;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Bundle;
@@ -30,6 +31,7 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 
@@ -60,15 +62,23 @@ public class PlayActivity extends AppCompatActivity implements AdapterView.OnIte
     AlertDialog myPopUpWinDialog;
 
     //This function is just a helper method -- to be deleted
-    public void getSelectedImages() {
-        selectedImages.add(new ImageDTO(R.drawable.laugh, BitmapFactory.decodeResource(this.getResources(), R.drawable.laugh)));
-        selectedImages.add(new ImageDTO(R.drawable.peep, BitmapFactory.decodeResource(this.getResources(), R.drawable.peep)));
+    public void getSelectedImages(List<String> filePaths) {
+        if (filePaths == null || filePaths.size() == 0) {
+            // FIXME: fallback images. remove it later.
+            selectedImages.add(new ImageDTO(R.drawable.laugh, BitmapFactory.decodeResource(this.getResources(), R.drawable.laugh)));
+            selectedImages.add(new ImageDTO(R.drawable.peep, BitmapFactory.decodeResource(this.getResources(), R.drawable.peep)));
 
-        selectedImages.add(new ImageDTO(R.drawable.snore, BitmapFactory.decodeResource(this.getResources(), R.drawable.snore)));
-        selectedImages.add(new ImageDTO(R.drawable.what, BitmapFactory.decodeResource(this.getResources(), R.drawable.what)));
+            selectedImages.add(new ImageDTO(R.drawable.snore, BitmapFactory.decodeResource(this.getResources(), R.drawable.snore)));
+            selectedImages.add(new ImageDTO(R.drawable.what, BitmapFactory.decodeResource(this.getResources(), R.drawable.what)));
 
-        selectedImages.add(new ImageDTO(R.drawable.tired, BitmapFactory.decodeResource(this.getResources(), R.drawable.tired)));
-        selectedImages.add(new ImageDTO(R.drawable.stop, BitmapFactory.decodeResource(this.getResources(), R.drawable.stop)));
+            selectedImages.add(new ImageDTO(R.drawable.tired, BitmapFactory.decodeResource(this.getResources(), R.drawable.tired)));
+            selectedImages.add(new ImageDTO(R.drawable.stop, BitmapFactory.decodeResource(this.getResources(), R.drawable.stop)));
+        } else {
+            for (int i = 0; i < filePaths.size(); i++) {
+                Bitmap bitmap = BitmapFactory.decodeFile(filePaths.get(i));
+                selectedImages.add(new ImageDTO(i, bitmap));
+            }
+        }
     }
 
     public void duplicateSelectedImages() {
@@ -82,9 +92,11 @@ public class PlayActivity extends AppCompatActivity implements AdapterView.OnIte
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
+        Intent intent = getIntent();
+        List<String> imagePaths = intent.getStringArrayListExtra("image_paths");
+        getSelectedImages(imagePaths);
 
         //This is all to be deleted
-        getSelectedImages();
         duplicateSelectedImages();
 
         //Shuffle the images so that the grid view has no adjacent same image
@@ -309,7 +321,7 @@ public class PlayActivity extends AppCompatActivity implements AdapterView.OnIte
         ImageView timerImage = (ImageView) findViewById(R.id.timerView);
         Animation animation = AnimationUtils.loadAnimation(this, R.anim.shake);
 
-        timer =new CountDownTimer(duration, 1000) {
+        timer = new CountDownTimer(duration, 1000) {
             @Override
             public void onTick(long millisUntilFinished) {
                 String timeLeft = String.format(Locale.ENGLISH, "%02d : %02d"
