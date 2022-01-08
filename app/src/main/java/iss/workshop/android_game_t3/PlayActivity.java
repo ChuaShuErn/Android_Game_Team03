2d12ac5391e808a6e0384926141c9816fbb5570a
Gabriel Wei Chen Kho <e0838413@u.nus.edu>
2022-01-08 20:29:01 +0800

    Changed the animation of flipping the card

--- app/src/main/java/iss/workshop/android_game_t3/PlayActivity.java
+++ app/src/main/java/iss/workshop/android_game_t3/PlayActivity.java
@@ -1,6 +1,7 @@
 package iss.workshop.android_game_t3;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -37,11 +38,11 @@ public class PlayActivity extends AppCompatActivity implements AdapterView.OnIte
     private final ArrayList<ImageDTO> gameImages = new ArrayList<>();
 
     //--- This variables are used in the method onitemClick
-    private ImageView image1=null;
-    private ImageView image2=null;
-    private int countMatchedPairs=0;
-    private int previousPosition=-1;
-    private int numOfSelectedImage=0;
+    private ImageView image1 = null;
+    private ImageView image2 = null;
+    private int countMatchedPairs = 0;
+    private int previousPosition = -1;
+    private int numOfSelectedImage = 0;
     private final ArrayList<Integer> matchedImagePositions = new ArrayList<>();
     TextView matchText;
     private long clickedStartTime;
@@ -57,19 +58,19 @@ public class PlayActivity extends AppCompatActivity implements AdapterView.OnIte
     AlertDialog myPopUpWinDialog;
 
     //This function is just a helper method -- to be deleted
-    public void getSelectedImages(){
-        selectedImages.add(new ImageDTO(R.drawable.laugh, BitmapFactory.decodeResource(this.getResources(),R.drawable.laugh)));
-        selectedImages.add(new ImageDTO(R.drawable.peep, BitmapFactory.decodeResource(this.getResources(),R.drawable.peep)));
+    public void getSelectedImages() {
+        selectedImages.add(new ImageDTO(R.drawable.laugh, BitmapFactory.decodeResource(this.getResources(), R.drawable.laugh)));
+        selectedImages.add(new ImageDTO(R.drawable.peep, BitmapFactory.decodeResource(this.getResources(), R.drawable.peep)));
 
-        selectedImages.add(new ImageDTO(R.drawable.snore, BitmapFactory.decodeResource(this.getResources(),R.drawable.snore)));
-        selectedImages.add(new ImageDTO(R.drawable.what, BitmapFactory.decodeResource(this.getResources(),R.drawable.what)));
+        selectedImages.add(new ImageDTO(R.drawable.snore, BitmapFactory.decodeResource(this.getResources(), R.drawable.snore)));
+        selectedImages.add(new ImageDTO(R.drawable.what, BitmapFactory.decodeResource(this.getResources(), R.drawable.what)));
 
-        selectedImages.add(new ImageDTO(R.drawable.tired, BitmapFactory.decodeResource(this.getResources(),R.drawable.tired)));
-        selectedImages.add(new ImageDTO(R.drawable.stop, BitmapFactory.decodeResource(this.getResources(),R.drawable.stop)));
+        selectedImages.add(new ImageDTO(R.drawable.tired, BitmapFactory.decodeResource(this.getResources(), R.drawable.tired)));
+        selectedImages.add(new ImageDTO(R.drawable.stop, BitmapFactory.decodeResource(this.getResources(), R.drawable.stop)));
     }
 
-    public void duplicateSelectedImages(){
-        for(ImageDTO imageDTO:selectedImages){
+    public void duplicateSelectedImages() {
+        for (ImageDTO imageDTO : selectedImages) {
             gameImages.add(imageDTO);
             gameImages.add(imageDTO);
         }
@@ -98,8 +99,8 @@ public class PlayActivity extends AppCompatActivity implements AdapterView.OnIte
         runnable = new Runnable() {
             @Override
             public void run() {
-                image1.setImageBitmap(BitmapFactory.decodeResource(PlayActivity.this.getResources(),R.drawable.dummy));
-                image2.setImageBitmap(BitmapFactory.decodeResource(PlayActivity.this.getResources(),R.drawable.dummy));
+                image1.setImageBitmap(BitmapFactory.decodeResource(PlayActivity.this.getResources(), R.drawable.dummy));
+                image2.setImageBitmap(BitmapFactory.decodeResource(PlayActivity.this.getResources(), R.drawable.dummy));
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
@@ -110,13 +111,11 @@ public class PlayActivity extends AppCompatActivity implements AdapterView.OnIte
         };
     }
 
-    protected void initGridView()
-    {
-        GridView gridView=(GridView) findViewById(R.id.gameGridView);
-        ImageAdapter imageAdapter=new ImageAdapter(this);
+    protected void initGridView() {
+        GridView gridView = (GridView) findViewById(R.id.gameGridView);
+        ImageAdapter imageAdapter = new ImageAdapter(this);
 
-        if (gridView!=null)
-        {
+        if (gridView != null) {
             gridView.setAdapter(imageAdapter);
             gridView.setOnItemClickListener(this);
         }
@@ -128,25 +127,25 @@ public class PlayActivity extends AppCompatActivity implements AdapterView.OnIte
 
         GridView gridView = findViewById(R.id.gameGridView);
         ViewGroup gridElement = (ViewGroup) gridView.getChildAt(position);
-        TextView scoreView=findViewById(R.id.score);
+        TextView scoreView = findViewById(R.id.score);
 
         //If no images are selected, set all dependent variables to null
-        if(numOfSelectedImage == 0){
+        if (numOfSelectedImage == 0) {
             previousPosition = -1;
             image1 = null;
             image2 = null;
         }
 
         //if the same image is selected twice or an already matched image is selected then return
-        if(position==previousPosition || matchedImagePositions.contains(position)) return;
+        if (position == previousPosition || matchedImagePositions.contains(position)) return;
 
         //This code handles the first image click
-        if(previousPosition<0 && image1 == null){
+        if (previousPosition < 0 && image1 == null) {
             numOfSelectedImage++;
             previousPosition = position;
             image1 = (ImageView) gridElement.getChildAt(0);
 
-            image1.animate().rotationYBy(720).rotationXBy(720).setDuration(200).withEndAction(new Runnable() {
+            image1.animate().rotationBy(360).setDuration(200).withEndAction(new Runnable() {
                 @Override
                 public void run() {
                     image1.setImageBitmap(gameImages.get(position).getBitmap());
@@ -156,14 +155,14 @@ public class PlayActivity extends AppCompatActivity implements AdapterView.OnIte
             clickedStartTime = System.currentTimeMillis();
         }
         //This code handles the second image click
-        else if(image1!=null && image2 == null){
+        else if (image1 != null && image2 == null) {
             image2 = (ImageView) gridElement.getChildAt(0);
 
             //If the image 1 and image 2 are same
-            if(gameImages.get(previousPosition).getBitmap()==gameImages.get(position).getBitmap()){
+            if (gameImages.get(previousPosition).getBitmap() == gameImages.get(position).getBitmap()) {
                 matchedImagePositions.add(previousPosition);
                 matchedImagePositions.add(position);
-                image2.animate().rotationYBy(720).rotationXBy(720).setDuration(200).withEndAction(new Runnable() {
+                image2.animate().rotationBy(360).setDuration(200).withEndAction(new Runnable() {
                     @Override
                     public void run() {
                         image2.setImageBitmap(gameImages.get(position).getBitmap());
@@ -173,17 +172,14 @@ public class PlayActivity extends AppCompatActivity implements AdapterView.OnIte
                 countMatchedPairs++;
                 matchText.setText(countMatchedPairs + " of " + selectedImages.size() + " images");
                 clickedEndTime = System.currentTimeMillis();
-                if ((clickedEndTime-clickedStartTime)<=5000)
-                {
-                    score+=5;
-                    if ((clickedEndTime-clickedStartTime)<=3000)
-                        score+=3;
-                }
-                else
-                    score+=3;
-            }
-            else{
-                image2.animate().rotationYBy(720).rotationXBy(720).setDuration(200).withEndAction(new Runnable() {
+                if ((clickedEndTime - clickedStartTime) <= 5000) {
+                    score += 5;
+                    if ((clickedEndTime - clickedStartTime) <= 3000)
+                        score += 3;
+                } else
+                    score += 3;
+            } else {
+                image2.animate().rotationBy(360).setDuration(200).withEndAction(new Runnable() {
                     @Override
                     public void run() {
                         image2.setImageBitmap(gameImages.get(position).getBitmap());
@@ -194,24 +190,22 @@ public class PlayActivity extends AppCompatActivity implements AdapterView.OnIte
             }
 
             //If the number of matched images is same as selected image display winner
-            if (countMatchedPairs==selectedImages.size())
-            {
-                Toast.makeText(getApplicationContext(),"You win!",Toast.LENGTH_SHORT).show();
+            if (countMatchedPairs == selectedImages.size()) {
+                Toast.makeText(getApplicationContext(), "You win!", Toast.LENGTH_SHORT).show();
                 PopUpWin();
             }
             numOfSelectedImage = 0;
         }
-        if (score<0)
-        {
+        if (score < 0) {
             //Implement GameOver function
         }
-        scoreView.setText("Score: "+score);
+        scoreView.setText("Score: " + score);
     }
 
-    public void PopUpWin(){
+    public void PopUpWin() {
         //inflate popUpWin layout into a view
         LayoutInflater inflater = (LayoutInflater) this.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
-        View myPopUpWin = inflater.inflate(R.layout.pop_up_win,null, false);
+        View myPopUpWin = inflater.inflate(R.layout.pop_up_win, null, false);
 
         //putting the view into a pop up
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
@@ -226,7 +220,7 @@ public class PlayActivity extends AppCompatActivity implements AdapterView.OnIte
         scoreTextView.setText("Your score is " + score);//need to get score from getScore()?
 
         Button submitBtn = myPopUpWin.findViewById(R.id.submitBtn);
-        if(submitBtn != null){
+        if (submitBtn != null) {
             submitBtn.setOnClickListener(this);
             submitBtn.setEnabled(false); //only set to true when the inputName is filled in
         }
@@ -240,23 +234,21 @@ public class PlayActivity extends AppCompatActivity implements AdapterView.OnIte
 
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                if(inputName.getText().toString().length() ==0){
-                    if(submitBtn!= null)
+                if (inputName.getText().toString().length() == 0) {
+                    if (submitBtn != null)
                         submitBtn.setEnabled(false); //disable submit button when inputName empty
-                }else
-                    if(submitBtn!= null)
-                        submitBtn.setEnabled(true);
+                } else if (submitBtn != null)
+                    submitBtn.setEnabled(true);
 
             }
 
             @Override //repeat implementation on TextChanged
             public void afterTextChanged(Editable editable) {
-                if(inputName.getText().toString().length() ==0){
-                    if(submitBtn!= null)
+                if (inputName.getText().toString().length() == 0) {
+                    if (submitBtn != null)
                         submitBtn.setEnabled(false); //disable submit button when inputName empty
-                }else
-                    if(submitBtn!= null)
-                        submitBtn.setEnabled(true);
+                } else if (submitBtn != null)
+                    submitBtn.setEnabled(true);
             }
         });
 
@@ -265,22 +257,22 @@ public class PlayActivity extends AppCompatActivity implements AdapterView.OnIte
     }
 
     @Override
-    public void onClick(View view){
+    public void onClick(View view) {
         int id = view.getId();
 
-        if(id ==R.id.submitBtn){
+        if (id == R.id.submitBtn) {
             //edit the share pre
             final SharedPreferences pref = getSharedPreferences("Leaderboard", MODE_PRIVATE);//initialize players.xml
 
-            int i=0;
+            int i = 0;
             //to detect last player inside player.xml
-            while(pref.contains("player"+i)){
+            while (pref.contains("player" + i)) {
                 i++;
             }
 
             SharedPreferences.Editor editor = pref.edit();
-            editor.putString("player"+i,inputName);//add nth player
-            editor.putInt("score"+i,score); //add nth player's score //need to get score from getScore()?
+            editor.putString("player" + i, inputName);//add nth player
+            editor.putInt("score" + i, score); //add nth player's score //need to get score from getScore()?
             editor.commit();
             //data/data/iss.workshop.android_game_t3/shared_prefs/Leaderboard.xml -->check shared pref here
 
@@ -295,29 +287,27 @@ public class PlayActivity extends AppCompatActivity implements AdapterView.OnIte
 
 
     @SuppressLint("SetTextI18n")
-    protected void initMatchView(){
+    protected void initMatchView() {
 
         matchText = findViewById(R.id.matchCounter);
         matchText.setText(countMatchedPairs + " of " + selectedImages.size() + " images");
     }
 
-    protected void startTimer()
-    {
-        TextView timerView=findViewById(R.id.timer);
-        long duration= TimeUnit.MINUTES.toMillis(1);
-        ImageView timer=(ImageView) findViewById(R.id.timerView);
-        Animation animation = AnimationUtils.loadAnimation(this,R.anim.shake);
+    protected void startTimer() {
+        TextView timerView = findViewById(R.id.timer);
+        long duration = TimeUnit.MINUTES.toMillis(1);
+        ImageView timer = (ImageView) findViewById(R.id.timerView);
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.shake);
 
         new CountDownTimer(duration, 1000) {
             @Override
             public void onTick(long millisUntilFinished) {
-                String timeLeft=String.format(Locale.ENGLISH,"%02d : %02d"
-                        ,TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished)
-                        ,TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished)-
-                        TimeUnit.MILLISECONDS.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished)));
+                String timeLeft = String.format(Locale.ENGLISH, "%02d : %02d"
+                        , TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished)
+                        , TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished) -
+                                TimeUnit.MILLISECONDS.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished)));
                 timerView.setText(timeLeft);
-                if(millisUntilFinished<10000)
-                {
+                if (millisUntilFinished < 10000) {
                     timerView.setTextColor(Color.RED);
                     timer.startAnimation(animation);
                 }
