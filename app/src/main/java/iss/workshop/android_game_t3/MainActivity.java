76384fe04725274738f648eb185086473c74c540
Gabriel Wei Chen Kho <e0838413@u.nus.edu>
2022-01-11 00:15:47 +0800

    Fix some bugs

--- app/src/main/java/iss/workshop/android_game_t3/MainActivity.java
+++ app/src/main/java/iss/workshop/android_game_t3/MainActivity.java
@@ -1,13 +1,10 @@
 package iss.workshop.android_game_t3;
 
-import android.content.ComponentName;
+
 import android.content.Intent;
-import android.content.ServiceConnection;
 import android.os.Bundle;
-import android.os.IBinder;
 import android.view.View;
 import android.widget.Button;
-import android.widget.MultiAutoCompleteTextView;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
@@ -93,7 +90,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 .setTitle("Really Exit?")
                 .setMessage("Are you sure you want to exit?")
                 .setNegativeButton(android.R.string.no, null)
-                .setPositiveButton(android.R.string.yes, ((arg0, arg1)-> super.onBackPressed())).create().show();
+                .setPositiveButton(android.R.string.yes, ((arg0, arg1)->{
+                    finishAffinity();
+                    System.exit(0);})).create().show();
     }
 
 //    //@Override
